b40f743df7cfda9f084ac9c4335356e4cca7826c
Miriali <miriali2907@gmail.com>
2021-04-06 18:14:28 -0400

    subiendo quiz

---
+++ README.md
@@ -0,0 +1,21 @@
+## Quiz sobre Hogwarts :zap: :school: :open_file_folder:
+
+Hola! espero que este muy bien :octocat:
+
+En este repositorio puedes encontrar el código de un quiz que pretende descubrir cuanto sabes sobre Hogwarts, esta desarrollado usando HTML, CSS y Javascript, me encanta como se ve :heart_eyes: y que las preguntas no sean sencillas en lo absoluto, adicionalmente son bastante legibles y todo el proceso de llenado del quiz es muy intuitivo.
+
+### Si quieres echarle un vistazo puedes hacerlo aqui :point_down:
+![]()
+
+### Tools
+- [Visual Studio Code](https://code.visualstudio.com/) :desktop_computer:
+- [Coolors](https://coolors.co/) :paintbrush:
+- [Box-Shadow](https://codepen.io/sdthornton/pen/wBZdXq) :black_nib:
+- [Contrast Checker](https://webaim.org/resources/contrastchecker/) :crayon:
+
+### Author :woman_technologist:
+[Miriali López Melean](https://github.com/Miriali) 
+
+### Stay In Touch :purple_heart:
+- [Codepen](https://codepen.io/your-work/) 
+- [Twitter](https://twitter.com/miricailopez)
---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "¿Donde está ubicado Hogwarts?",
+        answer: "Escocia",
+        options: [
+            "Irlanda",
+            "Bulgaria",
+            "Escocia",
+            "Estados Unidos"
+        ]
+    },
+    {
+        numb: 2,
+        question: "¿Donde se celebra la ceremonia de selección en Hogwarts?",
+        answer: "El Gran Comedor",
+        options: [
+            "La cámara secreta",
+            "Hogsmeade",
+            "El Gran Comedor",
+            "Sala de los Menesteres"
+        ]
+    },
+    {
+        numb: 3,
+        question: "¿Cual es el deporte favorito de los estudiantes de Hogwarts?",
+        answer: "Quidditch",
+        options: [
+            "Creaothceann",
+            "Swivenhodge",
+            "Ajedrez mágico",
+            "Quidditch"
+        ]
+    },
+    {
+        numb: 4,
+        question: "¿Cuales de las siguientes opciones no pertenece al sistema de casas de Hogwarts?",
+        answer: "Pukwudgie",
+        options: [
+            "Slytherin",
+            "Gryffindor",
+            "Pukwudgie",
+            "Hufflepuff"
+        ]
+    },
+    {
+        numb: 5,
+        question: "¿Cual de los siguientes personajes no fue director de Hogwarts?",
+        answer: "Nicolas Flamel",
+        options: [
+            "Albus Dumbledore",
+            "Minerva MacGonagall",
+            "Severus Snape",
+            "Nicolas Flamel"
+        ]
+    },
+    {
+        numb: 6,
+        question: "¿Cual de los siguientes hechizos protege a Hogwarts?",
+        answer: "Repello Muggletum",
+        options: [
+            "Avada Kedavra",
+            "Melofors",
+            "Repello Muggletum",
+            "Anapneo"
+        ]
+    },
+    {
+        numb: 7,
+        question: "¿Cuales son los colores de Ravenclaw?",
+        answer: "Azul y bronce",
+        options: [
+            "Azul y plata",
+            "Azul y rojo",
+            "Azul y bronce",
+            "Azul"
+        ]
+    },
+    {
+        numb: 8,
+        question: "¿Cuantas salas comunes tiene hogwarts?",
+        answer: "4",
+        options: [
+            "5",
+            "2",
+            "No tiene",
+            "4"
+        ]
+    },
+    {
+        numb: 9,
+        question: "¿Cual de las siguientes asignaturas no se estudia en Hogwarts?",
+        answer: "Astrología",
+        options: [
+            "Astrología",
+            "Defensa contra las artes oscuras",
+            "Aritmancia",
+            "Pociones"
+        ]
+    },
+    {
+        numb: 10,
+        question: "¿Quienes construyeron el expreso de Hogwarts?",
+        answer: "Ingenieros muggles",
+        options: [
+            "Elfos domésticos",
+            "Duendes",
+            "El ejército de Dumbledore",
+            "Ingenieros muggles"
+        ]
+    },
+];
